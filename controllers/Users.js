6cd4da49fe3162d2658a4b9ba134ce3a4610466c
berfin7 <berfinbasci4701@gmail.com>
2024-01-19 00:48:55 +0300

    update

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,34 +1,45 @@
 
 const User=require('../model/User')
+const SECRET_KEY='3apples'
+const Login= async (req,res)=>{
+try {
+    
+    const {email,password}=req.body
+    const user = await User.findOne({email})
 
-const Login=(req,res)=>{
-const {email,password}=req.body
-res.status(200).json({email,password})
-console.log("login success")
-console.log(email)
-//emal ile sorgula boyle bı emaıl var mı?
-//o emaıle sahıp kullanıcının sıfresını al
-//karsılastır
-//dogru ıse jwt ver yanlıs ıse hata ver
+    if(!user){
+        return res.status(401).json({error:'user did not avaliable'})
+    }
+    
+    const isEqual= await bcrypt.compare(password,user.password)
+    
+    if(!isEqual){
+        return res.status(401).json({error:'wrong password'})
+    }
 
-}
+    const token=jwt.sign({userId:user._id},SECRET_KEY,{expiresIn:'1h'})
+    res.status(200).json({token})
+    
+
+} catch (error) {
+    res.status(500).json({error:'Login failed'})
+}}
 
 
 const Register=async(req,res)=>{
 
-
-
-
  try{
-    console.log(req.body)
-            const user_register=await User.create(req.body)
-            res.status(200).json(user_register)
-            console.log("register succes")
+
+       const {userName,email,password}=req.body
+const hashedPassword=await bcrypt.hash(password,10)
+const user = new User({userName:userName,email:email,password:hashedPassword})
+await user.save()
+res.status(201).json({message:'user registered successfully'})
+
+
                 } 
                 catch(err){
-            console.log(err);
-            console.log("register failed")
-            console.log(user_register)
+        res.status(500).json({error: 'Registration failed'})
                 }   
              
 
